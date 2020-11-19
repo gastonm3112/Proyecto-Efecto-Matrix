e7b453069c6fc615d15c29247f0b0a9937d52e2c
Gaston Martinez <gastonm3112@gmail.com>
2020-11-19 08:27:54 -0300

    Creación de la Función InicializarMatrix

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ let height = document.body.clientHeight;
 
 elemento.width = width;
 elemento.height = height;
+
+let posicion = array(300).join(0).split('');
+
+function inicializarMatrix(){
+    contexto.fillStyle = 'rgba(0, 20, 1, 0.5)';
+    contexto.fillRect(0, 0, width, height);
+    contexto.fillStyle = '#37CC05';
+
+    contexto.font = '11pt arial';
+}
+
+setInterval(inicializarMatrix, 50);
