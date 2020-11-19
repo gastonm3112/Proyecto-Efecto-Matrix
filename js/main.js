3d2f4116ab6c3924e1be11ec199d4068ad94fffd
Gaston Martinez <gastonm3112@gmail.com>
2020-11-19 08:46:46 -0300

    Animación de caracteres

--- js/main.js
+++ js/main.js
@@ -6,14 +6,27 @@ let height = document.body.clientHeight;
 elemento.width = width;
 elemento.height = height;
 
-let posicion = array(300).join(0).split('');
+let posicion = Array(300).join(0).split('');
 
 function inicializarMatrix(){
-    contexto.fillStyle = 'rgba(0, 20, 1, 0.5)';
+    contexto.fillStyle = 'rgba(0, 30, 1, 0.5)';
     contexto.fillRect(0, 0, width, height);
     contexto.fillStyle = '#37CC05';
 
     contexto.font = '11pt arial';
+
+    posicion.map(function(y, index){
+        let texto = String.fromCharCode(1e2 + Math.random() * 30);
+        let x = (index * 15) + 15;
+
+        elemento.getContext('2d').fillText(texto, x, y);
+
+        if(y > 100 + Math.random() * 1e5){
+            posicion[index] = 0;
+        } else {
+            posicion[index] = y + 15;
+        }
+    })
 }
 
 setInterval(inicializarMatrix, 50);
