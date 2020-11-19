f63f83d85f7be3a75c4332da8cd9b57ad1ff22bb
Gaston Martinez <gastonm3112@gmail.com>
2020-11-19 08:16:06 -0300

    Obteniendo datos del Navegador en mi main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let elemento = document.getElementById('matrix');
+let contexto = elemento.getContext('2d');
+let width = document.body.clientWidth;
+let height = document.body.clientHeight;
+
+elemento.width = width;
+elemento.height = height;
